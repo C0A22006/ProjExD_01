7de84dff2fe1be57715f962da0a48afe0e242ecf
arita <c0a22006b2@edu.teu.ac.jp>
2023-04-18 15:57:04 +0900

    b

--- pygame_image.py
+++ pygame_image.py
@@ -11,16 +11,16 @@ def main():
     kk10_img = pg.transform.rotozoom(kk_img, 10, 1.0)
     kk_imgs = [kk_img,kk10_img]
 
-
     tmr = 0
 
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
+        
+        screen.blit(bg_img, [0,0])
+        tmr += 1 
+        screen.blit(kk_imgs[tmr%2], [300, 200])
 
-        tmr += 1
-        screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, [300, 200])
 
         pg.display.update()
         clock.tick(100)
